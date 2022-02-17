61c9813fdb04a1c33a73a053c98f57c522a6cc0e
onlyhappiness <onlyhappiness96@gmail.com>
2022-02-17 23:48:53 +0900

    Testing Phaser3

--- test/src/App.js
+++ test/src/App.js
@@ -1,14 +1,55 @@
 import './App.css';
-import { Stage, Sprite } from '@inlet/react-pixi';
+// import { Phaser, platforms } from 'phaser';
 
 function App() {
-  return (
-    <div className='App'>
-      {/* <Stage>
-        <Sprite image=
-      </Stage> */}
-    </div>
-  );
+  const config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    physics: {
+      default: 'arcade',
+      arcade: {
+        gravity: { y: 300 },
+        debug: false,
+      },
+    },
+    scene: {
+      preload: preload,
+      create: create,
+      update: update,
+    },
+  };
+
+  const game = new Phaser.Game(config);
+
+  function preload() {
+    this.load.image('sky', 'assets/sky.png');
+    this.load.image('ground', 'assets/platform.png');
+    this.load.image('star', 'assets/star.png');
+    this.load.image('bomb', 'assets/bomb.png');
+    this.load.spritesheet('dude', 'assets/dude.png', {
+      frameWidth: 32,
+      frameHeight: 48,
+    });
+  }
+
+  function create() {
+    this.add.image(400, 300, 'sky');
+
+    platforms = this.physics.add.staticGroup();
+
+    platforms.create(400, 568, 'ground').setScale(2).refreshBody();
+
+    platforms.create(600, 400, 'ground');
+    platforms.create(50, 250, 'ground');
+    platforms.create(750, 220, 'ground');
+
+    // this.add.image(400, 300, 'star');
+  }
+
+  function update() {}
+
+  return <div className='App'></div>;
 }
 
 export default App;
